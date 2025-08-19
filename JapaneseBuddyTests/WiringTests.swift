306ec2d07c6a0ac9b71c152cefac69caaff8fae1
ykcoatepe <121376377+ykcoatepe@users.noreply.github.com>
2025-08-19 08:22:00 +0300

    test: add WiringTests and wire into JapaneseBuddyProjTests target (create groups, no copy)

---
+++ JapaneseBuddyTests/WiringTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import JapaneseBuddyProj   // if your app target name differs, change this
+
+final class WiringTests: XCTestCase {
+    func testTargetWired() {
+        XCTAssertTrue(true) // just proves the test target runs
+    }
+}
+
