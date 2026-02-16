a7a1266775a05583137c78d089f3e91e9796163d
ykcoatepe <121376377+ykcoatepe@users.noreply.github.com>
2026-02-16 07:36:13 +0300

    test: add kanji decode regression coverage to lesson decode tests

--- JapaneseBuddyTests/LessonDecodeTests.swift
+++ JapaneseBuddyTests/LessonDecodeTests.swift
@@ -23,5 +23,46 @@ final class LessonDecodeTests: XCTestCase {
         XCTAssertEqual(loaded.lastStep, 2)
         XCTAssertEqual(loaded.stars, 3)
     }
+
+    func testKanjiWordsDecodeFromObjects() throws {
+        let json = #"""
+        {
+          "id": "T-1",
+          "title": "Test",
+          "canDo": "Decode",
+          "activities": [{"check": {}}],
+          "tips": [],
+          "kanjiWords": [
+            {"id": "T-1-水", "kanji": "水", "reading": "みず", "meaning": "water"}
+          ]
+        }
+        """#
+
+        let lesson = try JSONDecoder().decode(Lesson.self, from: Data(json.utf8))
+        XCTAssertEqual(lesson.kanjiWords?.count, 1)
+        XCTAssertEqual(lesson.kanjiWords?.first?.kanji, "水")
+        XCTAssertEqual(lesson.kanjiWords?.first?.reading, "みず")
+    }
+
+    func testKanjiWordsDecodeFromStringsBackCompat() throws {
+        let json = #"""
+        {
+          "id": "T-2",
+          "title": "Test",
+          "canDo": "Decode",
+          "activities": [{"check": {}}],
+          "tips": [],
+          "kanjiWords": ["水", "食"]
+        }
+        """#
+
+        let lesson = try JSONDecoder().decode(Lesson.self, from: Data(json.utf8))
+        XCTAssertEqual(lesson.kanjiWords?.count, 2)
+        XCTAssertEqual(lesson.kanjiWords?[0].id, "T-2-水")
+        XCTAssertEqual(lesson.kanjiWords?[0].kanji, "水")
+        XCTAssertEqual(lesson.kanjiWords?[0].reading, "")
+        XCTAssertEqual(lesson.kanjiWords?[0].meaning, "")
+    }
 }
+
 
